11fe357d1acf24418b97bfb723b7b84929c95871
ftwfabian <41267402+ftwfabian@users.noreply.github.com>
2023-11-05 00:01:38 -0500

    refreshing

--- parametrics.py
+++ parametrics.py
@@ -7,6 +7,14 @@ r = 1
 x = r * np.cos(t)
 y = r * np.sin(t)
 
+x_min = -5
+x_max = x_min * -1
+y_min = -5
+y_max = y_min * -1
+
+plt.xlim(x_min,x_max)
+plt.ylim(y_min,y_max)
 plt.plot(x,y)
+plt.grid(True, linestyle='--', linewidth = 0.5, color='gray')
 plt.axis('equal')
 plt.show()
