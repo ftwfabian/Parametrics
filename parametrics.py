217526b8a7cd93651abf2863c041fb57ea671d20
Harrison Fabian <harrisonfabian@Harrisons-Air.lan>
2023-11-11 09:59:09 -0600

    changes

--- parametrics.py
+++ parametrics.py
@@ -2,10 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 t = np.linspace(0, 2 * np.pi, 100)
+t2 = np.linspace(2*np.pi, 4*np.pi, 100)
 r = 1
 
-x = r * np.cos(t)
-y = r * np.sin(t)
+x1 = t
+x2 = t2
+
+y1 = np.sin(x1)
+y2 = np.cos(x2)
+
+x = np.concatenate((x1,x2))
+y = np.concatenate((y1,y2))
 
 x_min = -5
 x_max = x_min * -1
